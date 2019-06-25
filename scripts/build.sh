db19812ab0da3c04e382c4d0a1005a8ffafc0bb1
David Alger <davidmalger@gmail.com>
2019-06-25 12:50:02 -0500

    Reconfigured build script to build multiple versions of 2.3.x

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,12 +20,31 @@ SEARCH_PATH="${1:-*}"
 pushd ${BASE_DIR} >/dev/null
 docker login
 
+CURRENT_VERSION="2.3.2"   # used to generate 2.3 tag
+BUILD_VERSIONS="2.3.0 2.3.1 2.3.2"
+
 ## iterate over and build each Dockerfile
 for file in $(find ${SEARCH_PATH} -type f -name Dockerfile); do
-    BUILD_DIR="$(dirname "${file}")"
-    IMAGE_TAG="davidalger/magento:$(dirname "${file}" | tr / -)"
+  BUILD_DIR="$(dirname "${file}")"
+  COMPOSER_AUTH="$(cat "$(composer config -g home)/auth.json")"
 
-    export COMPOSER_AUTH="$(cat "$(composer config -g home)/auth.json")"
-    docker build -t "${IMAGE_TAG}" ${BUILD_DIR} --build-arg COMPOSER_AUTH
-    docker push "${IMAGE_TAG}"
+  for MAGENTO_VERSION in ${BUILD_VERSIONS}; do
+    IMAGE_TAGS=-t\ "davidalger/magento:${MAGENTO_VERSION}"
+    if [[ ! ${MAGENTO_VERSION} =~ ^$(basename $(dirname "${file}")) ]]; then
+      IMAGE_TAGS+=-$(basename $(dirname "${file}"))
+    fi
+
+    if [[ ${CURRENT_VERSION} = ${MAGENTO_VERSION} ]]; then
+      IMAGE_TAGS+=\ -t\ "davidalger/magento:$(dirname "${file}" | tr / -)"
+    fi
+
+    export COMPOSER_AUTH MAGENTO_VERSION
+    docker build ${IMAGE_TAGS} ${BUILD_DIR} --build-arg COMPOSER_AUTH --build-arg MAGENTO_VERSION
+    for tag in ${IMAGE_TAGS}; do
+      if [[ ${tag} = "-t" ]]; then
+        continue
+      fi
+      docker push ${tag}
+    done
+  done
 done
